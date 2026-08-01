cf27538e6a658bff630d2c02247158ec721856f9
arlethelvira <arlethelviraguerrero@gmail.com>
2026-08-01 15:40:35 -0600

    Entity y table

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -6,13 +6,13 @@
                 <script>
                     document.write(new Date().getFullYear())
                 </script>
-                &copy; Reback. Crafted by
+                &copy; Agendit. Crafted by
                 <iconify-icon
                     icon="iconamoon:heart-duotone"
                     class="fs-18 align-middle text-danger"
                 ></iconify-icon>
                 <a href="" class="fw-bold footer-text" target="_blank"
-                    >Techzaa</a
+                    >PAPOIS</a
                 >
             </div>
         </div>
